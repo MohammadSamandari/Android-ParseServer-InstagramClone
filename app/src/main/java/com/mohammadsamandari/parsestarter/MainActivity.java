c7533f0c54569578b320ce15309f434ced3ba2f6
Samandari Mohammad <samandari.mohammad@gmail.com>
2019-04-29 00:27:07 +0430

    Initial commit

---
+++ app/src/main/java/com/mohammadsamandari/parsestarter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mohammadsamandari.parsestarter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+    }
+}
